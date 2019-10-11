604c87371f1069a9b0ebc49961d76d70266ea780
Marc Kalil <m.coding@hotmail.com>
2019-10-11 15:43:37 +0200

    Timing

--- src/Timing/Timing.tsx
+++ src/Timing/Timing.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import Animated, { Easing } from "react-native-reanimated";
-import { loop } from "react-native-redash";
 import { useMemoOne } from "use-memo-one";
 
 import SimpleActivityIndicator from "./SimpleActivityIndicator";
@@ -18,7 +17,9 @@ const {
   stopClock,
   clockRunning,
   and,
-  not
+  not,
+  timing,
+  eq
 } = Animated;
 
 const styles = StyleSheet.create({
@@ -29,10 +30,57 @@ const styles = StyleSheet.create({
   }
 });
 
+const runTiming = (clock: Animated.Clock) => {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    frameTime: new Value(0),
+    time: new Value(0)
+  };
+  const config = {
+    toValue: new Value(1),
+    duration: 1000,
+    easing: Easing.linear
+  };
+  return block([
+    timing(clock, state, config),
+    cond(eq(state.finished, 1), [
+      set(state.finished, 0),
+      set(state.frameTime, 0),
+      set(state.time, 0),
+      set(config.toValue, cond(eq(state.position, 1), 0, 1))
+    ]),
+    state.position
+  ]);
+};
 export default () => {
+  const [playing, setPlaying] = useState(false);
+  const { isPlaying, progress, clock } = useMemoOne(
+    () => ({
+      progress: new Value(0),
+      clock: new Clock(),
+      isPlaying: new Value(0) as Animated.Value<number>
+    }),
+    []
+  );
+  isPlaying.setValue(playing ? 1 : 0);
+  useCode(
+    block([
+      cond(and(eq(isPlaying, 0), clockRunning(clock)), stopClock(clock)),
+      cond(and(eq(isPlaying, 1), not(clockRunning(clock))), startClock(clock)),
+      set(progress, runTiming(clock))
+    ]),
+    []
+  );
+
   return (
     <View style={styles.container}>
-      <SimpleActivityIndicator />
+      <SimpleActivityIndicator {...{ progress }} />
+      <Button
+        label={playing ? "Pause" : "Start"}
+        onPress={() => setPlaying(!playing)}
+        primary
+      />
     </View>
   );
 };
